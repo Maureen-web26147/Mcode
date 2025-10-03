d691e3060672c5c2d625e2740fd16a222cdb347c
Maureen-web26147-ops <kamau26147@gmail.com>
2025-10-03 12:22:26 +0300

    Add files via upload

---
+++ Units.C
@@ -0,0 +1,21 @@
+//simple c program(Hello world)single
+/*
+Name:Maureen N Kamau
+Reg:CT100/G/26147/25
+Description:Units for this semester
+*/
+
+#include<stdio.h>
+//main function
+int main () {
+	printf("UNITS FOR THIS SEMESTER:\n");
+	printf("Computer system and organization:\n");
+	printf("Fundamentals of ICT:\n");
+	printf("Essentials of accounting:\n");
+	printf("Analogue electronics:\n");
+	printf("Introduction to computer application:\n");
+	printf("Communication skills:\n");
+	printf("Mathematics of science:\n");
+	printf("Introduction to programming and algorithms:\n");
+	return 0;
+}	
