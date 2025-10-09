11ed4f15992a94ed17ef68248677044e311b48de
Maureen-web26147 <kamau26147@gmail.com>
2025-10-09 18:10:06 +0300

    Add files via upload

---
+++ For_loop.c
@@ -0,0 +1,17 @@
+// simple c program(Hello world)single
+/*
+Name:Maureen N Kamau
+Reg:CT100/G/26147/25
+Description:For loop
+*/
+
+#include<stdio.h>
+
+int main(){
+	//start,stop,step
+	// for loop from 100 to 50,decrementing by 1
+	for(int i=100;i>=50;i--){
+		printf("%d\n",i);
+	}
+	return 0;
+}
