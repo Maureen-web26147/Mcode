476cee487bbc37be082e44a2fbb22f3f02b729b2
Maureen-web26147 <kamau26147@gmail.com>
2025-11-06 16:55:47 +0300

    Add files via upload

---
+++ book_titles.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    FILE*file;
+    char title[100];
+
+    // open a file
+    file=fopen("borrowed_books.txt","a");
+    if (file==NULL){
+       printf("Error in opening file!\n");
+        return 1;
+    }
+
+    printf("Enter the book title:");
+   fgets(title,sizeof(title),stdin);
+
+   fprintf(file,"%s\n",title);
+
+   //close a file
+   fclose(file);
+
+   printf("Book title successfully stored!\n");
+
+   return 0;
+}
